c0ae80c8fcc728b4705a8eea5e7d395c58af5217
fukuchiharuki <fukuchi.haruki@gmail.com>
2019-09-23 18:39:17 +0900

    feat: base-hrefの変更と再作成だけ実行

--- pukiwikidump.sh
+++ pukiwikidump.sh
@@ -2,14 +2,14 @@
 
 export INDEX="http://localhost/wiki/index.php"
 
-#export BASE_HREF="/static/pukiwikidump/"
-export BASE_HREF="/"
+export BASE_HREF="/static/pukiwikidump/"
+#export BASE_HREF="/"
 
 export TEMPLATE_TITLE="ポケットがチケットでいっぱい（アーカイブ）"
 export TEMPLATE_DESCRIPTION="Tipsや調べたことなど、自分のために残したもの。"
 export TEMPLATE_AUTHOR="ふくちはるき"
 export TEMPLATE_HOMEPAGE="https://fukuchiharuki.me/"
 
-bash $(cd $(dirname $0) && pwd)/scrape.sh
-zip page.zip -r page
+#bash $(cd $(dirname $0) && pwd)/scrape.sh
+#zip page.zip -r page
 bash $(cd $(dirname $0) && pwd)/setup.sh
